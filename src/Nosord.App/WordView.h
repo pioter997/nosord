06f6b0d091b692a353d556dd5872b2df28153aaa
peter997 <p.nosorowski0820@gmail.com>
2023-01-06 11:55:37 +0100

    Update sizes

--- src/Nosord.App/WordView.h
+++ src/Nosord.App/WordView.h
@@ -83,20 +83,20 @@ namespace Nosord {
 			this->pButtons->Controls->Add(this->btnCancel);
 			this->pButtons->Controls->Add(this->btnSave);
 			this->pButtons->Dock = System::Windows::Forms::DockStyle::Bottom;
-			this->pButtons->Location = System::Drawing::Point(0, 206);
-			this->pButtons->Margin = System::Windows::Forms::Padding(4);
+			this->pButtons->Location = System::Drawing::Point(0, 266);
+			this->pButtons->Margin = System::Windows::Forms::Padding(5, 5, 5, 5);
 			this->pButtons->Name = L"pButtons";
-			this->pButtons->Size = System::Drawing::Size(434, 55);
+			this->pButtons->Size = System::Drawing::Size(579, 55);
 			this->pButtons->TabIndex = 0;
 			// 
 			// btnCancel
 			// 
 			this->btnCancel->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Right));
 			this->btnCancel->DialogResult = System::Windows::Forms::DialogResult::Cancel;
-			this->btnCancel->Location = System::Drawing::Point(210, 14);
-			this->btnCancel->Margin = System::Windows::Forms::Padding(4);
+			this->btnCancel->Location = System::Drawing::Point(406, 14);
+			this->btnCancel->Margin = System::Windows::Forms::Padding(5, 5, 5, 5);
 			this->btnCancel->Name = L"btnCancel";
-			this->btnCancel->Size = System::Drawing::Size(100, 28);
+			this->btnCancel->Size = System::Drawing::Size(75, 27);
 			this->btnCancel->TabIndex = 3;
 			this->btnCancel->Text = L"Anuluj";
 			this->btnCancel->UseVisualStyleBackColor = true;
@@ -105,10 +105,10 @@ namespace Nosord {
 			// 
 			this->btnSave->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Right));
 			this->btnSave->DialogResult = System::Windows::Forms::DialogResult::OK;
-			this->btnSave->Location = System::Drawing::Point(318, 14);
-			this->btnSave->Margin = System::Windows::Forms::Padding(4);
+			this->btnSave->Location = System::Drawing::Point(491, 14);
+			this->btnSave->Margin = System::Windows::Forms::Padding(5, 5, 5, 5);
 			this->btnSave->Name = L"btnSave";
-			this->btnSave->Size = System::Drawing::Size(100, 28);
+			this->btnSave->Size = System::Drawing::Size(75, 27);
 			this->btnSave->TabIndex = 2;
 			this->btnSave->Text = L"Zapisz";
 			this->btnSave->UseVisualStyleBackColor = true;
@@ -120,9 +120,10 @@ namespace Nosord {
 				| System::Windows::Forms::AnchorStyles::Right));
 			this->txtWord->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
-			this->txtWord->Location = System::Drawing::Point(12, 12);
+			this->txtWord->Location = System::Drawing::Point(13, 15);
+			this->txtWord->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->txtWord->Name = L"txtWord";
-			this->txtWord->Size = System::Drawing::Size(406, 23);
+			this->txtWord->Size = System::Drawing::Size(553, 27);
 			this->txtWord->TabIndex = 4;
 			this->txtWord->TabStop = false;
 			// 
@@ -133,22 +134,23 @@ namespace Nosord {
 				| System::Windows::Forms::AnchorStyles::Right));
 			this->rtbTranslation->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
-			this->rtbTranslation->Location = System::Drawing::Point(12, 40);
+			this->rtbTranslation->Location = System::Drawing::Point(13, 49);
+			this->rtbTranslation->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->rtbTranslation->Name = L"rtbTranslation";
-			this->rtbTranslation->Size = System::Drawing::Size(406, 159);
+			this->rtbTranslation->Size = System::Drawing::Size(553, 208);
 			this->rtbTranslation->TabIndex = 1;
 			this->rtbTranslation->Text = L"";
 			// 
 			// WordView
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(434, 261);
+			this->ClientSize = System::Drawing::Size(579, 321);
 			this->Controls->Add(this->rtbTranslation);
 			this->Controls->Add(this->txtWord);
 			this->Controls->Add(this->pButtons);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedToolWindow;
-			this->Margin = System::Windows::Forms::Padding(2);
+			this->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->Name = L"WordView";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterParent;
 			this->Text = L"S³owo";
